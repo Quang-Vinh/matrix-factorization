Added ALS for estimating parameters and fixed rmse calculation

src/baseline_model.py
@@ -2,7 +2,38 @@ import numba as nb
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
+
 from .utils import preprocess_data
+
+
+@nb.njit()
+def _rmse(
+    X: np.ndarray, global_mean: float, user_biases: np.ndarray, item_biases: np.ndarray
+):
+    """
+    Calculates root mean squared error for given data and model parameters
+
+    Args:
+        X (np.ndarray): Matrix with columns user, item and rating
+        global_mean (float): Global mean rating
+        user_biases (np.ndarray): User biases vector of shape (n_users, 1)
+        item_biases (np.ndarray): Item biases vector of shape (n_items, 1)
+
+    Returns:
+        float: Root mean squared error
+    """
+    n_ratings = X.shape[0]
+    errors = np.zeros(n_ratings)
+
+    # Iterate through all user-item ratings
+    for i in range(n_ratings):
+        user, item, rating = int(X[i, 0]), int(X[i, 1]), X[i, 2]
+        pred = global_mean + user_biases[user] + item_biases[item]
+        errors[i] = rating - pred
+
+    rmse = np.square(errors).mean()
+
+    return rmse
 
 
 @nb.njit()
@@ -17,10 +48,10 @@ def _sgd(
     verbose: int,
 ) -> (np.ndarray, np.ndarray):
     """
-    Performs stochastic gradient descent to estimate the user_biases and item_biases
+    Performs Stochastic Gradient Descent to estimate the user_biases and item_biases
 
     Arguments:
-        X {numpy array} -- User-item ranking matrix
+        X {numpy array} -- User-item rating matrix
         global_mean {float} -- Global mean of all ratings
         user_biases {numpy array} -- User biases vector of shape (n_users, 1)
         item_biases {numpy array} -- Item biases vector of shape (n_items, 1)
@@ -33,8 +64,9 @@ def _sgd(
         user_biases [np.ndarray] -- Updated user_biases vector
         item_biases [np.ndarray] -- Updated item_bases vector
     """
-
     for epoch in range(n_epochs):
+
+        # Iterate through all user-item ratings
         for i in range(X.shape[0]):
             user, item, rating = int(X[i, 0]), int(X[i, 1]), X[i, 2]
 
@@ -46,9 +78,88 @@ def _sgd(
             user_biases[user] += lr * (error - reg * user_biases[user])
             item_biases[item] += lr * (error - reg * item_biases[item])
 
-        # Display fitting messages
+        # Calculate error and print
         if verbose == 1:
-            rmse = error ** 2
+            rmse = _rmse(
+                X=X,
+                global_mean=global_mean,
+                user_biases=user_biases,
+                item_biases=item_biases,
+            )
+            print("Epoch ", epoch + 1, "/", n_epochs, " -  train_rmse:", rmse)
+
+    return user_biases, item_biases
+
+
+@nb.njit()
+def _als(
+    X: np.ndarray,
+    global_mean: float,
+    user_biases: np.ndarray,
+    item_biases: np.ndarray,
+    n_epochs: int,
+    reg: float,
+    verbose: int,
+) -> (np.ndarray, np.ndarray):
+    """
+    Performs Alternating Least Squares to estimate the user_biases and item_biases. For every epoch, the item biases are held constant while
+    solving directly for the user biases parameters using a closed form equation. Then the user biases paremeters is held constant and the same
+    is done for the item biases. This can be derived easily and is given in the lecture here https://www.youtube.com/watch?v=gCaOa3W9kM0&t=32m55s
+    which is also similar to the implementation in Surprise.
+
+    Arguments:
+        X {numpy array} -- User-item rating matrix
+        global_mean {float} -- Global mean of all ratings
+        user_biases {numpy array} -- User biases vector of shape (n_users, 1)
+        item_biases {numpy array} -- Item biases vector of shape (n_items, 1)
+        n_epochs {int} -- Number of epochs to run
+        reg {float} -- Regularization parameter lambda for Frobenius norm
+        verbose {int} -- Verbosity when fitting. 0 for nothing and 1 for printing epochs
+
+    Returns:
+        user_biases [np.ndarray] -- Updated user_biases vector
+        item_biases [np.ndarray] -- Updated item_bases vector
+    """
+    n_users = user_biases.shape[0]
+    n_items = item_biases.shape[0]
+
+    # For each epoch optimize User biases, and then Item biases
+    for epoch in range(n_epochs):
+
+        # Update user bias parameters
+        user_biases = np.zeros(n_users)
+        user_sizes = np.zeros(n_users)
+
+        # Iterate through all user-item ratings
+        for i in range(X.shape[0]):
+            user, item, rating = int(X[i, 0]), int(X[i, 1]), X[i, 2]
+            user_biases[user] += rating - global_mean - item_biases[item]
+            user_sizes[user] += 1
+
+        # Set user bias estimation
+        user_biases = user_biases / (reg + user_sizes)
+
+        # Update item bias parameters
+        item_biases = np.zeros(n_items)
+        item_sizes = np.zeros(n_items)
+
+        # Iterate through all user-item ratings
+        for i in range(X.shape[0]):
+            user, item, rating = int(X[i, 0]), int(X[i, 1]), X[i, 2]
+            item_biases[item] += rating - global_mean - user_biases[user]
+            item_sizes[item] += 1
+
+        # Set item bias estimation
+        item_biases = item_biases / (reg + item_sizes)
+
+        # Calculate error and print
+        if verbose == 1:
+            rmse = _rmse(
+                X=X,
+                global_mean=global_mean,
+                user_biases=user_biases,
+                item_biases=item_biases,
+            )
             print("Epoch ", epoch + 1, "/", n_epochs, " -  train_rmse:", rmse)
 
     return user_biases, item_biases
@@ -116,7 +227,7 @@ class BaselineModel(BaseEstimator):
     Arguments:
         n_epochs {int} -- Number of epochs to train for (default: {100})
         reg {float} -- Lambda parameter for L2 regularization (default: {0.2})
-        lr {float} -- Learning rate for gradient optimisation step (default: {0.005})
+        lr {float} -- Learning rate for gradient optimization step (default: {0.005})
         min_rating {int} -- Smallest rating possible (default: {0})
         max_rating {int} -- Largest rating possible (default: {5})
         verbose {str} -- Verbosity when fitting. 0 to not print anything, 1 to print fitting model (default: {1})
@@ -134,6 +245,7 @@ class BaselineModel(BaseEstimator):
 
     def __init__(
         self,
+        method: str = "sgd",
         n_epochs: int = 100,
         reg: float = 0.02,
         lr: float = 0.005,
@@ -141,6 +253,7 @@ class BaselineModel(BaseEstimator):
         max_rating: int = 5,
         verbose=1,
     ):
+        self.method = method
         self.n_epochs = n_epochs
         self.reg = reg
         self.lr = lr
@@ -170,17 +283,29 @@ class BaselineModel(BaseEstimator):
 
         self.global_mean = X["rating"].mean()
 
-        # Run stochastic gradient descent
-        user_biases, item_biases = _sgd(
-            X=X.to_numpy(),
-            global_mean=self.global_mean,
-            user_biases=self.user_biases,
-            item_biases=self.item_biases,
-            n_epochs=self.n_epochs,
-            lr=self.lr,
-            reg=self.reg,
-            verbose=self.verbose,
-        )
+        # Run parameter estimation
+        if self.method == "sgd":
+            self.user_biases, self.item_biases = _sgd(
+                X=X.to_numpy(),
+                global_mean=self.global_mean,
+                user_biases=self.user_biases,
+                item_biases=self.item_biases,
+                n_epochs=self.n_epochs,
+                lr=self.lr,
+                reg=self.reg,
+                verbose=self.verbose,
+            )
+
+        elif self.method == "als":
+            self.user_biases, self.item_biases = _als(
+                X=X.to_numpy(),
+                global_mean=self.global_mean,
+                user_biases=self.user_biases,
+                item_biases=self.item_biases,
+                n_epochs=self.n_epochs,
+                reg=self.reg,
+                verbose=self.verbose,
+            )
 
         return self
 