0033dd5f18014d6ba3329861e29c8e76edcd1ff7
Vinh <qvinhdo@gmail.com>
2020-06-19 17:35:24 -0400

    Add comment

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,25 @@ from sklearn.model_selection import train_test_split
 
 
 def train_update_test_split(X: pd.DataFrame, frac_new_users: float):
+    """
+    Split data into 3 parts (train_initial, train_update, test_update) for testing performance of model update for new users. First, a set of new
+    users is set and all ratings corresponding to all other users is assigned to train_initial. Then, for each new user half of their ratings are
+    stored in train_update and half are stored in test_update. 
+
+    To use the three sets returned:
+        1. Fit your model to train_update set.
+        2. Update your model with train_update 
+        3. Calculate predictions on test_update and compare with their actual ratings
+
+    Args:
+        X (pd.DataFrame): User-item ratings dataframe. Must contain a column user_id.
+        frac_new_users (float): Fraction of users to not include in train_initial
+
+    Returns:
+        train_initial [pd.DataFrame]: Training set for initial model fitting
+        train_update [pd.DataFrame]: Training set for model updating. Contains users that are not in train_initial
+        test_update [pd.DataFrame]: Testing set for model updating. Contains same users as train_update
+    """
     users = X["user_id"].unique()
 
     # Users that won't be included in the initial training
