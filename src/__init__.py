5ce9427eae75488a01e2dcfb004c0836af0ac5fd
Vinh <qvinhdo@gmail.com>
2020-07-16 20:00:22 -0400

    Expose RecommenderBase class for import

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,11 @@
 from .baseline_model import BaselineModel
 from .matrix_factorization import MatrixFactorization
+from .recommender_base import RecommenderBase
 from .utils import train_update_test_split
 
-__all__ = ["BaselineModel", "MatrixFactorization", "train_update_test_split"]
+__all__ = [
+    "BaselineModel",
+    "MatrixFactorization",
+    "RecommenderBase",
+    "train_update_test_split",
+]
